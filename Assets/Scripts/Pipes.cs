ae8038a36f427a545f78bfaf602a6cf1d8382e4c
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2023-04-18 09:08:17 -0400

    pipe movement

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{   
+    public float speed = 3.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //---Pipe Movement (Left)---
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+        //---Pipe Destroyer Offscreen---
+        if (transform.position.magnitude > 20f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
